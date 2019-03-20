793d0ef27b637adaf811b24252231e8213dd0a4a
Kneelawk <jedidiah.pommert@gmail.com>
2019-03-19 21:39:23 -0700

    Get basic window to open

--- src/main/java/com/kneelawk/hellovulkan/HelloVulkan.java
+++ src/main/java/com/kneelawk/hellovulkan/HelloVulkan.java
@@ -1,7 +1,39 @@
 package com.kneelawk.hellovulkan;
 
+import org.lwjgl.system.MemoryStack;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.Properties;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.vulkan.VK10.*;
+
 public class HelloVulkan {
-	public static void main(String[] args) {
-		System.out.println("Hello Vulkan");
+	public static void main(String[] args) throws IOException {
+//		System.getProperties().store(new FileOutputStream("system.properties"), null);
+
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		long window = glfwCreateWindow(1280, 720, "Hello Vulkan", 0, 0);
+
+		int extensionCount = 0;
+		try (MemoryStack stack = MemoryStack.stackPush()) {
+			IntBuffer buf = stack.ints(extensionCount);
+			vkEnumerateInstanceExtensionProperties((ByteBuffer) null, buf, null);
+			extensionCount = buf.get(0);
+		}
+
+		System.out.println(extensionCount + " extensions supported");
+
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+
+		glfwDestroyWindow(window);
+		glfwTerminate();
 	}
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,9 @@
 module com.kneelawk.hellovulkan {
     exports com.kneelawk.hellovulkan;
+
+    requires org.lwjgl;
+    requires org.lwjgl.natives;
+    requires org.lwjgl.glfw;
+    requires org.lwjgl.glfw.natives;
+    requires org.lwjgl.vulkan;
 }
