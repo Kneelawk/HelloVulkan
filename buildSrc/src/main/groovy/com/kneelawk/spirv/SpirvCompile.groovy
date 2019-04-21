6b625020ed1fbc88e1ed206e376d0181a032b3bb
Kneelawk <jedidiah.pommert@gmail.com>
2019-04-20 18:58:16 -0700

    Add SPIR-V shader compilation system

---
+++ buildSrc/src/main/groovy/com/kneelawk/spirv/SpirvCompile.groovy
@@ -0,0 +1,88 @@
+package com.kneelawk.spirv
+
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.file.Directory
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileCollection
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
+import org.gradle.process.ExecResult
+import org.gradle.process.internal.ExecAction
+import org.gradle.process.internal.ExecActionFactory
+
+import javax.inject.Inject
+
+class SpirvCompile extends DefaultTask {
+    private ExecAction execAction = execActionFactory.newExecAction()
+
+    @SkipWhenEmpty
+    @PathSensitive(PathSensitivity.RELATIVE)
+    @InputFiles
+    FileCollection inputFiles
+
+    @Input
+    Property<String> moduleName = objectFactory.property(String)
+
+    @Input
+    Property<String> modulePackage = objectFactory.property(String)
+
+    @OutputDirectory
+    DirectoryProperty outputDir = objectFactory.directoryProperty()
+
+    @Inject
+    protected ExecActionFactory getExecActionFactory() {
+        throw new UnsupportedOperationException()
+    }
+
+    @Inject
+    protected ObjectFactory getObjectFactory() {
+        throw new UnsupportedOperationException()
+    }
+
+    @TaskAction
+    void execute() {
+        Directory outputPackageDir = outputDir.dir(modulePackage.map { pac ->
+            pac.replace('.', '/')
+        }).get()
+
+        if (!outputPackageDir.asFile.exists()) {
+            outputPackageDir.asFile.mkdirs()
+        }
+
+        execAction.executable('glslangValidator')
+        execAction.workingDir(project.rootDir)
+
+        execAction.args('-V')
+        execAction.args(inputFiles.files)
+        execAction.args('-o', outputPackageDir.file(moduleName).get())
+
+        ByteArrayOutputStream standardOutputBuf = new ByteArrayOutputStream()
+        execAction.standardOutput = standardOutputBuf
+
+        ByteArrayOutputStream errorOutputBuf = new ByteArrayOutputStream()
+        execAction.errorOutput = errorOutputBuf
+
+        ExecResult result = execAction.execute()
+        String standardOutput = standardOutputBuf.toString()
+        String errorOutput = errorOutputBuf.toString()
+
+        if (result.exitValue != 0) {
+            throw new GradleException('SPIR-V compiler returned non-zero status')
+        }
+
+        errorOutput.lines().forEach { line ->
+            if (line.contains('ERROR')) {
+                throw new GradleException(line)
+            }
+        }
+
+        standardOutput.lines().forEach { line ->
+            if (line.contains('ERROR')) {
+                throw new GradleException(line)
+            }
+        }
+    }
+}
---
+++ buildSrc/src/main/groovy/com/kneelawk/spirv/SpirvCompilerPlugin.groovy
@@ -0,0 +1,71 @@
+package com.kneelawk.spirv
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
+import org.gradle.api.file.SourceDirectorySet
+import org.gradle.api.tasks.SourceSet
+
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class SpirvCompilerPlugin implements Plugin<Project> {
+    Project project
+
+    void apply(Project project) {
+        this.project = project
+
+        addSourceSetExtensions()
+        addCompileTasks()
+    }
+
+    void addSourceSetExtensions() {
+        project.sourceSets.all { sourceSet ->
+            String name = sourceSet.name
+            SourceDirectorySet sds = project.objects.sourceDirectorySet(name, "$name Spirv source")
+            sourceSet.extensions.add('spirv', sds)
+            sds.srcDir("src/$name/spirv")
+            sds.include("**/*.glsl")
+            sds.outputDir = new File(project.buildDir, "spirv/$name")
+        }
+    }
+
+    void addCompileTasks() {
+        project.sourceSets.all { sourceSet ->
+            addCompileTask(sourceSet)
+        }
+    }
+
+    void addCompileTask(SourceSet sourceSet) {
+        project.task([type: SpirvCompile], sourceSet.name == 'main' ? 'compileSpirv' : "compile${sourceSet.name.capitalize()}Spirv") {
+            inputFiles = project.files(sourceSet.spirv.files)
+            moduleName = "${sourceSet.name}.spv"
+            modulePackage = getCommonPackage(sourceSet.spirv.srcDirs, sourceSet.spirv.files)
+            outputDir = sourceSet.spirv.outputDir
+
+            description = "Compiles ${sourceSet.name} SPIR-V source."
+        }
+    }
+
+    static String getCommonPackage(Set<File> srcDirs, Set<File> files) {
+        // part of this code was taken from RosettaCode
+        // https://rosettacode.org/wiki/Find_common_directory_path#Groovy
+        return files.collect { file ->
+            Path filePath = file.toPath().getParent()
+            Path packagePath = Paths.get('')
+            for (File dir : srcDirs) {
+                Path dirPath = dir.toPath()
+                if (filePath.startsWith(dirPath)) {
+                    packagePath = dirPath.relativize(filePath)
+                }
+            }
+            packagePath.asList()
+        }.transpose().inject([match: true, commonParts: []]) { result, part ->
+            result.match = result.match && part.every { it == part[0] }
+            if (result.match) {
+                result.commonParts << part[0]
+            }
+            result
+        }.commonParts.join('.')
+    }
+}
