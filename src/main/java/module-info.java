ca85785a918de33affc4a748e46905fa9451ba60
Kneelawk <jedidiah.pommert@gmail.com>
2019-05-18 17:27:52 -0700

    Remove implementation dependency on lwjgl natives

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,6 @@ module com.kneelawk.hellovulkan {
     requires com.google.common;
     requires org.joml;
     requires org.lwjgl;
-    requires org.lwjgl.natives;
     requires org.lwjgl.glfw;
-    requires org.lwjgl.glfw.natives;
     requires org.lwjgl.vulkan;
 }
